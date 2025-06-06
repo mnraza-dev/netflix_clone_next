1e308ac2889f43559df938247554a9a82cd9affb
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-07 00:53:49 +0530

    Enhance Navbar component by adding mobile and desktop menu layouts with navigation links for improved user experience

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -17,6 +17,30 @@ export default function Navbar() {
                         width={100}
                         height={100} />
                 </Link>
+                {/* Mobile Menu */}
+                <div className="md:hidden">
+                    {/* Hamburger Icon */}
+                </div>
+                {/* Desktop Menu */}
+                <ul className="hidden md:flex space-x-4">
+                    <li className='text-sm text-gray-200 hover:underline'>
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li className='text-sm text-gray-200 hover:underline'>
+                        <Link href="/">Movies</Link>
+                    </li>
+                    <li className='text-sm text-gray-200 hover:underline'>
+                        <Link href="/">TV Shows</Link>
+                    </li>
+
+                    <li className='text-sm text-gray-200 hover:underline'>
+                        <Link href="/">Latest</Link>
+                    </li>
+
+                </ul>
+                <div className='font-light items-center space-x-4 text-sm md:space-x-6'>
+                    <SearchIcon className='hidden h-6 w-6 md:inline' />
+                </div>
 
             </nav>
         </header>
